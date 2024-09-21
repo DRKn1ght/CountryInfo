4c22c829d569ff258217037b9e6b85fb7c760a42
Guilherme Panobianco <guipanobianc@gmail.com>
2024-09-21 18:08:54 -0300

    feat: added country info page with charts
    only with sample data

--- my-next-app/frontend/src/models/country.model.ts
+++ my-next-app/frontend/src/models/country.model.ts
@@ -3,3 +3,18 @@ export interface Country {
     countryName: string;
     countryFlag: string;
 }
+
+export interface PopulationCount {
+    year: number;
+    value: number;
+}
+
+export interface DetailedCountryInfo {
+    countryCode: string;
+    countryName: string;
+    countryOfficialName:string
+    countryFlag: string;
+    region: string;
+    borders: Country[];
+    populationCounts: PopulationCount[];
+}
